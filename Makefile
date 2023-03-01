6ac5300787b730e67bfe978e0394f094869f9a9e
MarinaS <m_shnabel@mail.ru>
2023-03-01 23:00:38 +0300

    Name and group have added, Makefile has fixed

--- Makefile
+++ Makefile
@@ -1,18 +1,19 @@
 all: lab3
 
 lab3: lab3.o libflow.so
-  gcc -o lab3 lab3.o -L. lflow
+  	gcc -o lab3 lab3.o -L. lflow
 
 lab3.o: lab3.c
-  gcc -c lab3.c
+  	gcc -c lab3.c
 
 libflow.so: flow.o
-  gcc -shared -o libflow.so flow.o
+  	gcc -shared -o libflow.so flow.o
 
 flow.o: flow.c
-  gcc -c -fPIC flow.c
+  	gcc -c -fPIC flow.c
 
-DEBUG: -g
+debug: all
+		make --debug
 
 clean:
-  rm -f*.o *.so lab3
+  	rm -f*.o *.so lab3
--- flow.c
+++ flow.c
@@ -9,16 +9,15 @@ int counter=0;
 pthread_mutex_t mutex;
 
 void *long_thread (void *notused)
-
 {
   int n;
   for(n=0; n<5; n++)
   {
-pthread_mutex_lock(&mutex);
-counter++;
-pthread_mutex_unlock(&mutex);
-printf("It’s the first flow, TID %d - N of repeat %d \n", thread_id1, n);
-sleep(2);
+    pthread_mutex_lock(&mutex);
+    counter++;
+    pthread_mutex_unlock(&mutex);
+    printf("Marina, TID %d - N of repeat %d \n", thread_id1, n);
+    sleep(2);
   }
 }
 
@@ -27,7 +26,7 @@ void *long_thread2(void *notused)
   int m;
   for(m=0; m<5; m++)
   {
-  	printf("It’s the second flow, TID %d - N of repeat %d \n", thread_id2, m);
-sleep(1);
+  	printf("Shnabel I992, TID %d - N of repeat %d \n", thread_id2, m);
+    sleep(1);
   }
 }
